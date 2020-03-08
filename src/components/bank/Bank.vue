a5b5c74cce982f7c4aa0f42eec129148bc0d7643
Karolina Bogacka <kbogacka1@wp.pl>
2020-03-08 13:31:15 +0100

    Removed vuetify from components: Contact,Gdpr, Bank

--- src/components/bank/Bank.vue
+++ src/components/bank/Bank.vue
@@ -1,34 +1,11 @@
 <template>
-  <v-container
-    fluid
-  >
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col
-        cols="12"
-        sm="8"
-        md="4"
-      >
-        <v-card class="elevation-12">
-          <v-toolbar
-            color="primary"
-            dark
-            flat
-          >
-            <v-toolbar-title>Konto do opłat</v-toolbar-title>
-            <v-spacer />
-          </v-toolbar>
-          <v-card-text>
-            <div><strong>Nazwa operatora:</strong> {{bank.nazwa}}}</div>
-            <div><strong>Numer konta:</strong> {{bank.konto}}}</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-</template>
+  <div>
+    <h1>Konto do opłat</h1>
+    <div><label>Nazwa operatora:</label> {{bank.nazwa}}}</div>
+    <div><label>Numer konta:</label> {{bank.konto}}}</div>
+  </div>
+
+  </template>
 
 <script>
   import { mapState, mapActions } from 'vuex';
--- src/components/gdpr/Gdpr.vue
+++ src/components/gdpr/Gdpr.vue
@@ -1,45 +1,10 @@
 <template>
-  <v-container
-    fluid
-  >
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col
-        cols="12"
-        sm="8"
-        md="4"
-      >
-        <v-card class="elevation-12">
-          <v-toolbar
-            color="primary"
-            dark
-            flat
-          >
-            <v-toolbar-title>Informacja RODO</v-toolbar-title>
-            <v-spacer />
-          </v-toolbar>
-          <v-card-text>
-            <div style={padding}>Jako {{gdpr.name}}}, w świetle przepisów jesteśmy, administratorem Państwa danych osobowych. Oznacza to, że odpowiadamy za ich wykorzystanie w sposób bezpieczny, zgodny z umową oraz obowiązującymi przepisami.</div>
-            <div style={padding}>Proszę kliknąć następujący link aby przeczytać treść komunikatu <a :href="'/rodo-' + gdpr.company + '.pdf'" target='_blank'>TREŚĆ KOMUNIKATU</a></div>
-            <div style={padding}><Button variant="raised" color="primary" onClick="confirm">Potwierdzam zapoznanie się</Button></div>
-
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="primary"
-              class="mr-4"
-              @click="confirm"
-            >
-              Potwierdzam zapoznanie się
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+<div>
+    <h1>Informacja RODO</h1>
+    <div style={padding}>Jako {{gdpr.name}}, w świetle przepisów jesteśmy, administratorem Państwa danych osobowych. Oznacza to, że odpowiadamy za ich wykorzystanie w sposób bezpieczny, zgodny z umową oraz obowiązującymi przepisami.</div>
+    <div style={padding}>Proszę kliknąć następujący link aby przeczytać treść komunikatu <a :href="'/rodo-' + gdpr.company + '.pdf'" target='_blank'>TREŚĆ KOMUNIKATU</a></div>
+    <div style={padding}><button> Potwierdzam zapoznanie się</button></div>
+</div>
 </template>
 
 <script>
